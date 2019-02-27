dac07d033c3e815757ab74fa486951bf71d32f36
Chalermchai <ebiz_niit5@scg.com>
2019-02-27 17:13:39 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import data from "./jsonData/holidays.json";
 
 class App extends Component {
   render() {
+    console.log(data.holidays);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Holidays!</h1>
+        {data.holidays.map((item, index) => (
+          <div key={index} className="holiday">
+            <span>Topic: {item.publicHolidayName}</span>
+            <span>
+              📅 วัน {item.dayOfWeek} ท่ี {item.publicHolidayDate}
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
