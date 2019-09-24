863e4153614bd3e406e2b18cb2495a65eee7a884
flatnote <flatsnote@gmail.com>
2019-09-24 23:24:47 +0700

    render by backend data

--- src/App.js
+++ src/App.js
@@ -17,104 +17,6 @@ const scgAPI =
 
 const dataBaseAPI = "https://line-noti-bot.herokuapp.com/api/holidays";
 
-function sortFirstToLast(listData) {
-  return listData.sort(function(first, second) {
-    const dateFirst = new Date(first.publicHolidayDate),
-      dateSecond = new Date(second.publicHolidayDate);
-    return dateFirst - dateSecond;
-  });
-}
-
-const srcImgs = [
-  {
-    src:
-      "https://i.pinimg.com/564x/e9/e6/e8/e9e6e8d32e784d3ebdb2259d2016dfb6.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/2c/64/65/2c6465f406fe78a4dcbb9ee898793b26.jpg",
-    titleClass: "card_title title-white",
-    bodyClass: "card-text title-white"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/8c/d0/a7/8cd0a7dd98109ad4554e9ab62171435b.jpg",
-    titleClass: "card_title title-white",
-    bodyClass: "card-text title-white"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/4f/12/01/4f1201475f701da83a0958f6dadd57a7.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/e6/65/d4/e665d4c8f993cc059091dd4c247a2183.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/19/30/86/19308619265bd811961997bbe9f30373.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/df/35/77/df3577ff067e0c7e2289fb7c30090c19.jpg",
-    titleClass: "card_title title-white",
-    bodyClass: "card-text title-white"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/eb/49/b4/eb49b4459725d04923287b916edb841d.jpg",
-    titleClass: "card_title title-white",
-    bodyClass: "card-text title-white"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/f6/d4/19/f6d41913be89809aae005189ce9ec8ee.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/a2/bb/f8/a2bbf8cfe59bda4fc8890ad28460cff8.jpg",
-    titleClass: "card_title title-white",
-    bodyClass: "card-text title-white"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/e3/8d/b2/e38db2ef17616edeee08e05e0910bf4f.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  },
-  {
-    src:
-      "https://i.pinimg.com/564x/1e/f0/e5/1ef0e535626599dcfca4625c0ce7522b.jpg",
-    titleClass: "card_title title-black",
-    bodyClass: "card-text title-black"
-  }
-];
-
-const template = [
-  { month: "January", data: [] },
-  { month: "February", data: [] },
-  { month: "March", data: [] },
-  { month: "April", data: [] },
-  { month: "May", data: [] },
-  { month: "June", data: [] },
-  { month: "July ", data: [] },
-  { month: "August", data: [] },
-  { month: "September", data: [] },
-  { month: "October", data: [] },
-  { month: "November ", data: [] },
-  { month: "December", data: [] }
-];
-
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -122,14 +24,13 @@ class Main extends Component {
     this.state = {
       holidaysData: [],
       yearSeleced: moment().year(),
-      cardData: template,
+      cardData: [],
       options: [],
       loading: false
     };
 
     this.selectChange = this.selectChange.bind(this);
-    this.groupMonth = this.groupMonth.bind(this);
-    this.prepareOption = this.prepareOption.bind(this);
+    // this.prepareOption = this.prepareOption.bind(this);
     this.getHolidayData = this.getHolidayData.bind(this);
     this.synchronizeData = this.synchronizeData.bind(this);
   }
@@ -159,7 +60,7 @@ class Main extends Component {
 
   getDatabase = () => {
     const { yearSeleced } = this.state;
-    const url = `${dataBaseAPI}/all?year=${yearSeleced}`;
+    const url = `${dataBaseAPI}/all?year=${yearSeleced}&grouping=true`;
     return axios.get(url).then(response => response.data);
   };
 
@@ -172,22 +73,7 @@ class Main extends Component {
   getHolidayData = async () => {
     this.setState({ loading: true });
     const data = await this.getDatabase();
-    this.setState(
-      { holidaysData: sortFirstToLast(data), loading: false },
-      this.groupMonth
-    );
-  };
-
-  groupMonth = () => {
-    const { holidaysData, cardData } = this.state;
-    this.setState({
-      cardData: cardData.map((item, index) => {
-        item.data = holidaysData.filter(
-          holiday => moment(holiday.publicHolidayDate).get("month") === index
-        );
-        return item;
-      })
-    });
+    this.setState({ cardData: data, loading: false });
   };
 
   selectChange(event) {
@@ -222,24 +108,21 @@ class Main extends Component {
         alt="loading"
       />
     ) : (
-      <div key="animate1" style={{ marginBottom: "5em" }}>
+      <div key="animate1">
         <Slider {...settings}>
           {cardData.map((item, index) => {
             return (
               <div className="card" key={item.month}>
                 <div className="card_image">
-                  <img src={srcImgs[index].src} alt={`card ${index}`} />{" "}
+                  <img src={item.bgUrl} alt={`card ${index}`} />{" "}
                 </div>
-                <div className={srcImgs[index].titleClass}>
+                <div className={item.titleClass}>
                   <span role="img" aria-label="Calendar">
                     📅
                   </span>
                   {` ${item.month}`}
                 </div>
-                <div
-                  className={srcImgs[index].bodyClass}
-                  style={{ minHeight: 100 }}
-                >
+                <div className={item.bodyClass} style={{ minHeight: 100 }}>
                   <ul>
                     {item.data.map(subItem => (
                       <li key={subItem.publicHolidayDate}>
@@ -261,46 +144,46 @@ class Main extends Component {
     );
   };
 
-  prepareOption = () => {
-    const min = moment().year() - 10;
-    const max = moment().year() + 10;
+  // prepareOption = () => {
+  //   const min = moment().year() - 10;
+  //   const max = moment().year() + 10;
 
-    let optionComponent = [];
-    for (let index = min; index < max; index++) {
-      optionComponent.push(index);
-    }
+  //   let optionComponent = [];
+  //   for (let index = min; index < max; index++) {
+  //     optionComponent.push(index);
+  //   }
 
-    return optionComponent.map(item => (
-      <option key={item} value={item}>
-        {item}
-      </option>
-    ));
-  };
+  //   return optionComponent.map(item => (
+  //     <option key={item} value={item}>
+  //       {item}
+  //     </option>
+  //   ));
+  // };
 
   render() {
     return (
       <div className="container" style={{ padding: 20 }}>
         <div className="row">
-          <div className="col">
+          <div className="col col-sm-2 col-lg-2">
             <h1>Holidays!</h1>
           </div>
-          <div className="col">
-            <select
+          <div className="refash-btn col offset-sm-4 col-sm-6 offset-lg-8 col-lg-2">
+            {/* <select
               value={this.state.yearSeleced}
               onChange={this.selectChange}
               className="browser-default custom-select"
               style={{ width: 100 }}
             >
               {this.prepareOption()}
-            </select>
-            <button
+            </select> */}
+            {/* <button
               type="button"
               className="btn btn-primary"
               onClick={this.getHolidayData}
               style={{ marginLeft: 10 }}
             >
               Change year
-            </button>
+            </button> */}
             {/* <img
               className="synchronize-btn"
               src="https://img.icons8.com/flat_round/32/000000/synchronize.png"
@@ -319,6 +202,7 @@ class Main extends Component {
         </div>
 
         <QueueAnim delay={300}>{this.renderCardGroup()}</QueueAnim>
+
         <footer>
           <div className="footer">
             <p>
