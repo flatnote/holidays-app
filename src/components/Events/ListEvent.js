976c3983533a99e640d12d37deb62a26ac950f9a
flatnote <flatsnote@gmail.com>
2019-11-24 22:46:11 +0700

    fix missing thumbnail

--- src/components/Events/ListEvent.js
+++ src/components/Events/ListEvent.js
@@ -1,24 +1,18 @@
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import Collapse from "@material-ui/core/Collapse";
 import { red } from "@material-ui/core/colors";
 import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import clsx from "clsx";
-import React, { Component } from "react";
-import { withFirebase } from "../Firebase";
-
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import axios from "axios";
+import React, { Component } from "react";
+import { withFirebase } from "../Firebase";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -71,7 +65,6 @@ function RecipeReviewCard(props) {
 
   const { cardData } = props;
   const { imgData } = cardData;
-  console.log(imgData.url);
   return (
     <Card className={classes.card}>
       <CardHeader
@@ -114,7 +107,7 @@ function RecipeReviewCard(props) {
       />
       <CardMedia
         className={classes.media}
-        image={imgData.url}
+        image={imgData.download_url}
         title="Paella dish"
       />
       <CardContent>
